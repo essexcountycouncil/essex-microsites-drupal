e4a6d7797443da827b6e119aca48eedd9e4f754c
Anthony Lindsay <anthony@annertech.com>
2024-03-29 13:37:03 +0100

    Swap incoming lat/long values in migration

---
+++ web/modules/custom/ecc_waste/src/Plugin/migrate/process/EccSwapValues.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\ecc_waste\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Process plugin to customize the location field.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "ecc_swap_values"
+ * )
+ */
+class EccSwapValues extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrateExecutable, Row $row, $destinationProperty) {
+    // Split the value into A and B.
+    list($a, $b) = explode(',', $value);
+
+    // Concatenate the elements.
+    $result = $b . ',' . $a;
+
+    return $result;
+  }
+
+}
