b6edd4c842812468219f8672d48992d78e5424c0
Anthony Lindsay <anthony@annertech.com>
2024-05-16 11:35:43 +0200

    Display a random did you know

---
+++ web/themes/custom/love_essex/js/did-you-know.js
@@ -0,0 +1,22 @@
+
+(function didYouKnow(Drupal) {
+  Drupal.behaviors.didYouKnow = {
+    attach: function (context) {
+      // Find all Did You Know content.
+      const dykContent = once('js-processed', '.paragraph--type--did-you-know', context);
+      // Pick one randomly.
+      if (dykContent.length > 1) {
+        selectedDyk = Math.floor(Math.random() * dykContent.length);
+        // Hide the rest.
+        for (var i = 0; i < dykContent.length; i++) {
+          if (i !== selectedDyk) {
+            dykContent[i].remove();
+          }
+          else {
+            dykContent[i].style.display = 'block';
+          }
+        }
+      }
+    },
+  };
+})(Drupal);
