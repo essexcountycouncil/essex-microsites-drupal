938b4ee31307ae66a2ba40b100a350506cf839e1
Anthony Lindsay <anthony@annertech.com>
2025-04-01 09:36:57 +0000

    Add HDRC custom theme

---
+++ web/themes/custom/hdrc/js/hdrc.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * HDRC behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.hdrc = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
